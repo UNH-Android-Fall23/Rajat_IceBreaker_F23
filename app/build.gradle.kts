51a75e367bce890da010dba255c90c2e270011f7
sihtodstel <rajat.olhan@outlook.com>
2023-09-09 19:23:29 -0400

    Gradle updated for bindings, gitignore updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,10 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    //googles method of variable binding - links kt to xml
+    buildFeatures{
+        viewBinding=true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -40,6 +43,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
