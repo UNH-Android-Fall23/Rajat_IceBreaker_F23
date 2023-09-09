36e2c75ce0228364cbb2f19f1a78280b7a81b451
sihtodstel <rajat.olhan@outlook.com>
2023-09-09 19:30:53 -0400

    binding variable creating and text updated

--- app/src/main/java/com/example/rajat_icebreaker_f23/MainActivity.kt
+++ app/src/main/java/com/example/rajat_icebreaker_f23/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.rajat_icebreaker_f23
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.rajat_icebreaker_f23.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var textView: TextView
+    private lateinit var binding : ActivityMainBinding
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //textView=findViewById(R.id.welcomeText)
+        //textView.setText("Testing Variable Overload with Hardcoded string")
+
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView((binding.root))
+        binding.welcomeText.text="setting text via binding ... Binding maybe cool"
     }
 }
+
