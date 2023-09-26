a75854c6716c04278187faa9ca8bb35424835856
sihtodstel <rajat.olhan@outlook.com>
2023-09-26 11:18:37 -0400

    Firebase linked and validated

--- app/src/main/java/com/example/rajat_icebreaker_f23/MainActivity.kt
+++ app/src/main/java/com/example/rajat_icebreaker_f23/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import com.example.rajat_icebreaker_f23.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     //private lateinit var textView: TextView
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     //private var sampleText: String = getString(R.string.sample_text)
     private val TAG= "IceBreakerAndroidF23Tag"
 
+    private val gDB=Firebase.firestore
+    private var questionBank: MutableList<Questions> = arrayListOf()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +24,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //textView=findViewById(R.id.welcomeText)
-        //textView.setText("Testing Variable Overload with Hardcoded string")
+
+
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView((binding.root))
         //binding.Header0.text= "IceBreaker"
+        getQuesFromFirebase()
 
         binding.buttonGetQuestion.setOnClickListener {// fetching questions on getquestions button click
             Log.d(TAG,"Button pressed")
-            getQuesFromFirebase()
+
+            binding.resultQues.setText(questionBank?.random().toString())
         }
 
         binding.buttonSubmit.setOnClickListener { //container for actions on submit click
@@ -41,8 +47,12 @@ class MainActivity : AppCompatActivity() {
 
             Log.d(TAG,"Info gathered is ${firstName.text} ,  ${lastName.text},  ${prefName.text},  ${answer.text}")
 
+            writeStudentToFirebase(binding)
+            firstName.setText("")
+            lastName.setText("")
+            prefName.setText("")
+            answer.setText("")
         }
-
 
     }
 
@@ -53,10 +63,49 @@ class MainActivity : AppCompatActivity() {
     //function to fetch questions from firebase
     private fun getQuesFromFirebase(){
         Log.d(TAG,"Fetching questions from DB.")
+        gDB.collection("questions")
+            .get()
+            .addOnSuccessListener { documents ->
+                questionBank= mutableListOf()
+                for(document in documents){
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    val question = document.toObject(Questions::class.java)
+                    questionBank!!.add(question) ?: "Empty Database"
+                    
+                }
+                }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
 
     }
 
 
+    private fun writeStudentToFirebase(binding: ActivityMainBinding){
+
+        val firstName=binding.inputFirstName
+        val lastName=binding.inputLastName
+        val prefName=binding.inputPrefName
+        val answer=binding.textAnswer
+
+        val student= hashMapOf("firstname" to firstName.text.toString(),"lastname" to lastName.text.toString(), "prefName" to prefName.text.toString(), "answer" to answer.text.toString())
+
+        gDB.collection("students")
+            .add(student)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "Docuemnt successfully written with ID ${documentReference.id}")
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
+
+
+
+
 
 }
+
+
+
 
--- app/src/main/java/com/example/rajat_icebreaker_f23/Questions.kt
+++ app/src/main/java/com/example/rajat_icebreaker_f23/Questions.kt
@@ -1,6 +1,6 @@
 package com.example.rajat_icebreaker_f23
 
-data class Questions(val text: String="") {
+data class Questions(val questionText: String="") {
     //staging fetch from firebase to display random qeus
 
 
